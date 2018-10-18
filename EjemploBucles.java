ff34c463e226160d391b6f4ad4e640aadfd77886
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-10-18 17:05:45 +0200

    Add files via upload

--- EjemploBucles.java
+++ EjemploBucles.java
@@ -27,5 +27,48 @@ public class EjemploBucles {
         }while (!numeroEsValido);
         JOptionPane.showMessageDialog(null,"Has introducido el "+numeroLeido);
 
+        //uso del for
+
+        //tres partes: inicialización, comprobación y actualización
+
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
+        //contar hacia atras
+
+        for (int i=10;i>0;i--){
+            System.out.println(i);
+        }
+
+        //Cuando dos variables estan muy relacionadas
+
+        for (int i=0, j=10;i<10;i++,j--){
+            System.out.println(i+" "+j);
+        }
+
+        //Podemos saber el numero de vueltas aunque no sea un numero fijo
+        int numeroVueltas=Integer.parseInt(JOptionPane.showInputDialog("Introduce numero vueltas"));
+        for (int i=0;i<=numeroVueltas;i++){
+            System.out.println(i);
+        }
+
+        //Bucle infinito
+
+        /*for (;;){
+            System.out.println("Hola esto es infinito");
+        }*/
+
+        //Ejemplo bucles anidados
+
+        int lado=Integer.parseInt(JOptionPane.showInputDialog("Tamaño del lado"));
+        for (int i=0;i<lado;i++){
+            for (int j = 0; j <lado ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
     }
 }
