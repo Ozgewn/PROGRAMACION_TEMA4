aab14ca940e3ea5ef9b55dcc493ce994c0ee5cc5
descomkraneoz <36744638+descomkraneoz@users.noreply.github.com>
2018-10-25 09:07:58 +0200

    Add files via upload

--- Reloj.java
+++ Reloj.java
@@ -5,32 +5,31 @@ public class Reloj {
     private int hora;
     private int minutos;
     private int segundos;
-    public char miSeparador=':';
+    public static char SEPARADOR=':';
+    private static int CONTADOR=0;
+
 
     public Reloj(int hora, int minutos, int segundos) {
         this.hora = hora;
         this.minutos = minutos;
         this.segundos = segundos;
-        this.miSeparador=getMiSeparador();
-        contador++;
+        CONTADOR++;
     }
 
     public Reloj(){
         this.hora=0;
         this.minutos=0;
         this.segundos=0;
-        this.miSeparador=getMiSeparador();
-        contador++;
+        this.SEPARADOR =getSEPARADOR();
+        CONTADOR++;
     }
-    private static int contador=0;
 
-    public static String totalRelojes() {
-
-        return ""+contador;
+    public static int totalRelojes() {
+        return CONTADOR;
     }
 
     public static String esValida(int hora, int minutos, int segundos) {
-        String salida="";
+        String salida;
 
         boolean horaValida=hora>0 && hora<59;
         boolean minutosValidos=minutos>0 && minutos<59;
@@ -45,8 +44,9 @@ public class Reloj {
         return salida;
     }
 
-    public static char cambiaSeparador(char miSeparador) {
-        return miSeparador;
+    public static char cambiaSeparador(char separador) {
+        Reloj.SEPARADOR = separador;
+        return separador;
     }
 
     public int getHora() {
@@ -73,28 +73,32 @@ public class Reloj {
         this.segundos = segundos;
     }
 
-    public char getMiSeparador() {
-        return miSeparador;
+    public static char getSEPARADOR() {
+        return SEPARADOR;
     }
 
-    public void setMiSeparador(char miSeparador) {
-        this.miSeparador = miSeparador;
+    public static void setSEPARADOR(char SEPARADOR) {
+        Reloj.SEPARADOR = SEPARADOR;
     }
 
     public String devuelveHora(){
         String salida="";
-        salida+=getHora()+""+getMiSeparador();
-        salida+=getMinutos()+""+getMiSeparador();
+        salida+=" "+getHora()+""+getSEPARADOR();
+        salida+=getMinutos()+""+getSEPARADOR();
         salida+=""+getSegundos();
         return salida;
     }
 
-    public String leeHora(){
-        hora= Integer.parseInt(JOptionPane.showInputDialog("Introduce hora de 0 a 23: "));
-        minutos= Integer.parseInt(JOptionPane.showInputDialog("Introduce minutos de 0 a 59: "));
-        segundos= Integer.parseInt(JOptionPane.showInputDialog("Introduce segundos de 0 a 59: "));
-        String leeHora=hora+""+minutos+""+segundos;
-        return leeHora;
+    public String leeHora() {
+        setHora(Integer.parseInt(JOptionPane.showInputDialog("Escribe la hora con formato de 0 a 23")));
+        setMinutos(Integer.parseInt(JOptionPane.showInputDialog("Escribe los minutos con formato de 0 a 59")));
+        setSegundos(Integer.parseInt(JOptionPane.showInputDialog("Escribe los segundos con formato de 0 a 59")));
+
+        String salida = "";
+        salida+=getHora();
+        salida+=getMinutos();
+        salida+=getSegundos();
+        return salida;
     }
 
     public void ponHora(int hora,int minutos){
@@ -111,10 +115,16 @@ public class Reloj {
 
 
     public void sumaTiempo(int hora, int minutos, int segundos) {
-        this.hora=hora+getHora();
-        this.minutos=minutos+getMinutos();
-        this.segundos=segundos+getSegundos();
+        this.segundos=getSegundos()+segundos;
+        if(this.segundos>=60){
+            this.segundos=getSegundos()-60;
+            minutos++;
+        }
+        this.minutos=getMinutos()+minutos;
+        if (this.minutos>=60){
+            this.minutos=getMinutos()-60;
+            hora++;
+        }
+        this.hora=getHora()+hora;
     }
-
-
 }
